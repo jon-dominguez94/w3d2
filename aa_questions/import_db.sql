accb49fc30b9c2c7abf6798fc513c96087509c02
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-11-06 11:03:25 -0800

    Create question_follows table

--- aa_questions/import_db.sql
+++ aa_questions/import_db.sql
@@ -8,7 +8,22 @@ CREATE TABLE questions (
   id INTEGER PRIMARY KEY,
   title TEXT,
   body TEXT,
+  user_id INTEGER NOT NULL,
   
-  FOREIGN KEY () REFERENCES ()
-)
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
 
+CREATE TABLE question_follows (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+  
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+
+
+
+# two foreigns keys for the link (references users & questions)
+# 
