4a344dad58154acf2e333ad4900be184b071bad4
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-11-06 10:55:49 -0800

    Create questions table

--- aa_questions/import_db.sql
+++ aa_questions/import_db.sql
@@ -1,7 +1,14 @@
 CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
   fname TEXT NOT NULL, 
   lname TEXT NOT NULL
 );
 
-
+CREATE TABLE questions (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  body TEXT,
+  
+  FOREIGN KEY () REFERENCES ()
+)
 
