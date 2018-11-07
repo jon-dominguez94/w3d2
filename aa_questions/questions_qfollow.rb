0f6f435705b988ae716e827fd8218df925992dd4
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-11-06 17:19:09 -0800

    Create #followers_for_question_id method

--- aa_questions/questions_qfollow.rb
+++ aa_questions/questions_qfollow.rb
@@ -17,4 +17,37 @@ class QuestionFollow
     return nil if follow.length == 0
     QuestionFollow.new(follow.first)
   end
+  
+  
+  def self.followers_for_question_id(@question_id)
+    #return an array of user objects
+    
+    # what_is_user_id = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+    #   SELECT user_id FROM question_follows WHERE question_id = ?    
+    # SQL
+    # 
+    # users_arr = QuestionsDatabase.instance.execute(<<-SQL, what_is_user_id)
+    #   SELECT * FROM users WHERE id = ?    
+    # SQL
+    # 
+    # return nil if users_arr.empty?
+    # users_arr.map {|u| User.new(u)}
+    
+    users = QuestionsDatabase.instance.execute(<<-SQL, @question_id)
+      SELECT 
+        u.id, u.fname, u.lname 
+      FROM 
+        users AS u 
+      JOIN
+        question_follows AS f
+      WHERE 
+        question_id = ? 
+        AND 
+          u.id = f.user_id
+    SQL
+    
+    return nil if users.empty?
+    users.map {|u| User.new(u)}  
+  end
+  
 end
